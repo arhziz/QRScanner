d9b95d84529563535f98e9045e82c8cd54826255
arhzi <arhzi@AZEEZ>
2022-03-05 16:36:55 +0100

    Add project files.

---
+++ QRScanner/QRScanner/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QRScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                string theResult = result.Text + " the result" + result.BarcodeFormat.ToString();
+                Application.Current.MainPage.DisplayAlert("QRCode Result", theResult, "OK");
+
+            });
+        }
+    }
+}
